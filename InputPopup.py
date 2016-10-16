9379b99a0563bb1c706226de1455b69e208bb44a
Mike Schwager <mike@schwager.com>
2016-10-16 07:56:05 -0500

    Added copyright information.

--- InputPopup.py
+++ InputPopup.py
@@ -1,3 +1,23 @@
+#
+#   Copyright 2016 Michael Anthony Schwager
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+# This is the InputPopup class. It presents something like a traditional
+# modal dialog box to the Kivy programmer, including a TextInput widget
+# so you may request text input from the end user. The text obtained is
+# returned to a callback function in the calling class.
+
 from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
 
